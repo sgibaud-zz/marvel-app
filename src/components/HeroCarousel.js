0b39e38ba9de7e080103ecc40be8c38700aa860c
Mylene CARRARA <mylene.carrara33@gmail.com>
2021-01-02 19:29:24 +0100

    nav bar et Hero Carousel

---
+++ src/components/HeroCarousel.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./HeroCarousel.css";
+
+
+class HeroCarousel extends Component {
+    render () {
+        return (
+            <Carousel>
+            <Carousel.Item interval={2000}>
+              <img
+                className="d-block w-100"
+                src="https://shopdisneyeu.scene7.com/is/image/DisneyStoreES/34819_marvel_mania_tb?$tb$"
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item interval={2000}>
+              <img
+                className="d-block w-100"
+                src="https://shopdisneyeu.scene7.com/is/image/DisneyStoreES/34819_marvel_mania_tb?$tb$"
+                alt="Third slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item interval={2000}>
+              <img
+                className="d-block w-100"
+                src="https://shopdisneyeu.scene7.com/is/image/DisneyStoreES/34819_marvel_mania_tb?$tb$"
+                alt="Third slide"
+              />
+            </Carousel.Item>
+          </Carousel>
+        )
+    }
+}
+
+
+export default HeroCarousel;
---
+++ src/components/SideNavBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./layout.css";
+import CloseIcon from '@material-ui/icons/Close';
+
+
+const SideNavBar = ({ close, display }) => {
+  let Classes = "side-nav-bar";
+  if (display) {
+    Classes = "side-nav-bar open";
+  }
+  return (
+    <nav className={Classes}>
+      <ul>
+        <li onClick={close}>
+          <CloseIcon />
+        </li>
+        <input id="searchBarMobile" type="text" placeholder="Rechercher"></input>
+        <li><a href="/">Accueil</a></li>
+        <li><a href="/">Personnages</a></li>
+        <li><a href="/">Créateurs</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default SideNavBar;
